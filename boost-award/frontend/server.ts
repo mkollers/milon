2a76146ffa4847510c8506d452787962014b3d12
Markus Kollers <m.kollers@vertical.de>
2019-09-22 16:42:27 +0200

    Add compression and hide powered-by header

--- boost-award/frontend/server.ts
+++ boost-award/frontend/server.ts
@@ -17,11 +17,14 @@
 
 import 'zone.js/dist/zone-node';
 
+import * as compression from 'compression';
 import * as express from 'express';
 import {join} from 'path';
 
 // Express server
 const app = express();
+app.use(compression());
+app.disable('x-powered-by');
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist/browser');
