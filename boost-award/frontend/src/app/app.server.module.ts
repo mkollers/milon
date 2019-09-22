b2fe670b7d4ec8d373f94d39f8d353c912435142
Markus Kollers <m.kollers@vertical.de>
2019-09-22 18:51:36 +0200

    Add universal interceptor

--- boost-award/frontend/src/app/app.server.module.ts
+++ boost-award/frontend/src/app/app.server.module.ts
@@ -4,6 +4,8 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UniversalInterceptor } from './shared/helper/universal-interceptor';
 
 @NgModule({
   imports: [
@@ -12,5 +14,10 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
     ModuleMapLoaderModule,
   ],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }]
 })
 export class AppServerModule {}
