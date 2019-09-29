c348fc531fd34aae1fd9366716c516d3d0d0fe54
Markus Kollers <m.kollers@vertical.de>
2019-09-29 16:53:04 +0200

    Fix build

--- boost-award/frontend/src/app/shared/helper/pipes/safe.pipe.spec.ts
+++ boost-award/frontend/src/app/shared/helper/pipes/safe.pipe.spec.ts
@@ -1,8 +1,17 @@
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { DomSanitizerMock } from '@mocks/dom-sanitizer.mock';
 import { SafePipe } from './safe.pipe';
 
 describe('SafePipe', () => {
+  let domSanitizer: DomSanitizer;
+
+  beforeEach(() => {
+    domSanitizer = new DomSanitizerMock();
+  });
+
   it('create an instance', () => {
-    const pipe = new SafePipe();
+    const pipe = new SafePipe(domSanitizer);
     expect(pipe).toBeTruthy();
   });
 });
---
+++ boost-award/frontend/src/mocks/dom-sanitizer.mock.ts
@@ -0,0 +1,8 @@
+export class DomSanitizerMock {
+    bypassSecurityTrustHtml = () => null;
+    bypassSecurityTrustResourceUrl = () => null;
+    bypassSecurityTrustScript = () => null;
+    bypassSecurityTrustStyle = () => null;
+    bypassSecurityTrustUrl = () => null;
+    sanitize = () => null;
+}
